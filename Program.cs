988fe3824505c9d77bac2d3c0185b968e12a06dc
Roie <roihemo32321@gmail.com>
2024-02-26 16:53:35 +0200

    End of section 7

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using dating_backend.Extensions;
+using dating_backend.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +14,8 @@ builder.Services.AddIdentityServices(builder.Configuration); // // Custom extens
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>(); // Using the middleware we created for error handling.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
