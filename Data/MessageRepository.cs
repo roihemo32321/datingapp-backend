a85eb4b30f9ff048dbd926b8fb6bbee1a12fe75a
Roie <roihemo32321@gmail.com>
2024-03-27 00:16:23 +0200

    Added new delete function

--- Data/MessageRepository.cs
+++ Data/MessageRepository.cs
@@ -41,9 +41,9 @@ namespace dating_backend.Data
 
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username),
-                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username),
-                _ => query.Where(u => u.RecipientUsername == messageParams.Username && u.DateRead == null),
+                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username && u.RecipientDeleted == false),
+                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username && u.SenderDeleted == false),
+                _ => query.Where(u => u.RecipientUsername == messageParams.Username && u.RecipientDeleted == false && u.DateRead == null),
             };
 
             var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
@@ -58,9 +58,9 @@ namespace dating_backend.Data
                 .Include(u => u.Sender).ThenInclude(p => p.Photos)
                 .Include(u => u.Recipient).ThenInclude(p => p.Photos)
                 .Where(
-                    m => m.RecipientUsername == currentUsername &&
+                    m => m.RecipientUsername == currentUsername && m.RecipientDeleted == false &&
                     m.SenderUsername == recipientUsername ||
-                    m.RecipientUsername == recipientUsername &&
+                    m.RecipientUsername == recipientUsername && m.SenderDeleted == false &&
                     m.SenderUsername == currentUsername)
                 .OrderBy(m => m.MessageSent)
                 .ToListAsync();
